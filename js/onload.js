b1c71d38e31c3888215f383ac32e64f0573010aa
Stefan Mihajlovic <stanleyserbia@gmail.com>
2022-02-13 23:12:19 +0100

    update

--- js/onload.js
+++ js/onload.js
@@ -5,7 +5,7 @@ function onLoadEn(){
     var typed = new Typed('.hero-description', {
         strings: ["Next generation fire protection materials.", "Passive fire protection (PFP) products based on a unique formula protected by an internationally granted patent."
         , "Best-In-Class plasticizer based on Green technology.", "Extreme performance confirmed in field conditions.", "Eco-friendly and time-efficient passive fire protection (PFP) products."],
-        typeSpeed: 40
+        typeSpeed: 30
     });
     $('.typed-cursor').remove();
     document.getElementById('year').textContent = new Date().getFullYear();
@@ -18,7 +18,7 @@ function onLoadSr(){
     var typed = new Typed('.hero-description', {
         strings: ["Nova generacija materijala za protivpožarnu zaštitu.", "Proizvodi bazirani na jedinstvenoj formuli zaštićenoj od strane međunarodno priznatog patenta.",
         "\"Best-in-class\" plastifikator baziran na Zelenoj tehnologiji.", "Odlične performanse potvrđene u terenskim uslovima.", "Ekološki i vremenski efikasni proizvodi za pasivnu protivpožarnu zaštitu."],
-        typeSpeed: 40
+        typeSpeed: 30
     });
     $('.typed-cursor').remove();
     document.getElementById('year').textContent = new Date().getFullYear();
