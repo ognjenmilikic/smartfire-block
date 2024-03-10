81550fecf3b5586b583afff95f3b3b9f0a65567b
ognjenmilikic <ognjenm@examroom.ai>
2024-03-10 21:12:03 +0100

    Adding our clients section.

--- js/onload.js
+++ js/onload.js
@@ -5,7 +5,9 @@ function onLoadEn(){
     var typed = new Typed('.hero-description', {
         strings: ["Next generation fire protection materials.", "Product based on unique manufacturing process aimed to reduce carbon footprint."
         , "Best-In-Class plasticizer based on Green technology.", "Extreme performance confirmed in field conditions.", "Production process in line with circular economy."],
-        typeSpeed: 30
+        backSpeed: 30,
+        typeSpeed: 30,
+        loop: true
     });
     $('.typed-cursor').remove();
     document.getElementById('year').textContent = new Date().getFullYear();
@@ -18,7 +20,9 @@ function onLoadSr(){
     var typed = new Typed('.hero-description', {
         strings: ["Nova generacija materijala za protivpožarnu zaštitu.", "Proizvodi bazirani na jedinstvenom procesu proizvodnje koji smanjuju emisiju CO₂.",
         "\"Best-in-class\" plastifikator baziran na Zelenoj tehnologiji.", "Odlične performanse potvrđene u terenskim uslovima.", "Proizvodni proces orijentisan na cirkularnu ekonomiju."],
-        typeSpeed: 30
+        backSpeed: 30,
+        typeSpeed: 30,
+        loop: true
     });
     $('.typed-cursor').remove();
     document.getElementById('year').textContent = new Date().getFullYear();
